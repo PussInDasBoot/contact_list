3242a25c42aa42e393ae5f522b12fdf95b843393
yemelyn <pommy.shortcake@gmail.com>
2016-07-05 23:58:36 +0000

    updated after code review

--- contact.rb
+++ contact.rb
@@ -1,99 +1,98 @@
-require 'csv'
 require 'pry'
+require 'pg'
 
-CONTACTS = "BC_contacts.csv"
+TABLE_NAME = "contacts"
 # Represents a person in an address book.
-# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
-  # @@ids = 0
-  attr_accessor :name, :email, :id
+  attr_accessor :name, :email
+  attr_reader :id
   
   # Creates a new contact object
-  # @param name [String] The contact's name
-  # @param email [String] The contact's email address
+  # @param['string'] is accessing the corresponding value of the 'string' key in the hash of a row of contacts table
   def initialize(name, email, id = nil)
     @name = name
     @email = email
     @id = id
-    # TODO: Assign parameter values to instance variables.
   end
+
+  @@conn = nil
+  @@from_hash_lambda = lambda { |row| Contact.new(row['name'], row['email'], row['id']) }
 
   # Formats the instances of a new contact nicely
   def to_s
-    "#{self.id}: #{self.name} (#{self.email})"
+    "#{id}: #{name} (#{email})"
   end
 
-  # Provides functionality for managing contacts in the csv file.
-  class << self
+  def self.connection
+      @@conn = @@conn || PG.connect(
+        dbname: 'contact_list'
+      )
+  end 
 
-    # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
-    # @return [Array<Contact>] Array of Contact objects
-    def all
-      bc_contacts = []
-      CSV.foreach(CONTACTS) do |row|
-        bc_contacts << Contact.new(row[1], row[2], row[0])
-      end
-      bc_contacts
-      # TODO: Return an Array of Contact instances made from the data in 'contacts.csv'.
-    end
-
-    # Creates a new contact, adding it to the csv file, returning the new contact.
-    # @param name [String] the new contact's name
-    # @param email [String] the contact's email
-    def create(name, email)
-      # Instantiates a Contact
-      # new_contact_array = []
-      new_id = CSV.read(CONTACTS).length + 1
-      new_contact = Contact.new(name, email, new_id)
-      # new_contact_array << new_contact.id
-      # new_contact_array << new_contact.name
-      # new_contact_array << new_contact.email
-      # add it's data to the contacts.csv file
-      CSV.open(CONTACTS, 'a+') do |csv|
-        csv << [new_contact.id, new_contact.name, new_contact.email]
-      end
-      # Return value
-      new_contact
-      # TODO: Instantiate a Contact, add its data to the 'contacts.csv' file, and return it.
-    end
-    
-    # Find the Contact in the 'contacts.csv' file with the matching id.
-    # @param id [Integer] the contact id
-    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
-    def find(id)
-      array = self.all
-      array.find_all{ |contact| contact.id == id }
-      # show = []
-      # CSV.foreach('CONTACTS') do |row|
-      #   show << Contact.new(row[1], row[2]) if row[0] == id
-      # end
-      # show
-      # TODO: Find the Contact in the 'contacts.csv' file with the matching id.
-    end
-    
-    # Search for contacts by either name or email.
-    # @param term [String] the name fragment or email fragment to search for
-    # @return [Array<Contact>] Array of Contact objects.
-    def search(term)
-      # gives an array of arrays, array containing array which represents each row in csv
-      row_array = CSV.read(CONTACTS)
-      # row is an array
-      row_results = row_array.find_all do |row|
-        # are there any rows where any of the values include that term
-        row.any? { |value| value.include?(term) }
-      end
-      # row results is an array of arrays
-      row_results.map do |row|
-        Contact.new(row[1], row[2], row[0])
-      end
-      # search = []
-      # CSV.foreach('BC_contacts.csv') do |row|
-      #   search << Contact.new(row[1], row[2]) if row.to_s.match(/#{term}/)
-      # end
-      # search
-      # TODO: Select the Contact instances from the 'contacts.csv' file whose name or email attributes contain the search term.
-    end
-
+  # Connects to local database and returns all entries in contact table
+  def self.all
+    res = self.connection.exec_params(
+      "SELECT * 
+      FROM #{TABLE_NAME}
+      ORDER BY id;")
+    res.map &@@from_hash_lambda
   end
 
+  # Creates a new contact
+  def self.create(name, email)
+    a = Contact.new(name, email)
+    a.save
+    a
+  end
+  # Finds a contact based on their id
+  def self.find(id)
+    result = self.connection.exec_params(
+      "SELECT * 
+      FROM #{TABLE_NAME} 
+      WHERE id = $1::int;", [id])
+    result = result[0]
+    if result
+      Contact.new(result['name'], result['email'], result['id'])
+    end
+  end
+  
+  # Should return any contacts that include a search term
+  def self.search(term)
+    result = self.connection.exec_params(
+      "SELECT * 
+      FROM #{TABLE_NAME} 
+      WHERE name LIKE '%' || $1 || '%';", [term])
+    result.map &@@from_hash_lambda
+  end
+
+  def save
+    if id #UPDATE
+      self.class.connection.exec_params(
+        "UPDATE #{TABLE_NAME} 
+        SET name = $1, email = $2 
+        WHERE id =$3::int;", [name, email, id])
+    elsif self.class.connection.exec_params(
+        "SELECT * 
+        FROM #{TABLE_NAME} 
+        WHERE email = $1;", [email]).ntuples > 0
+      raise EmailExists, 'Email already exists, try unique email'
+    else
+      res = self.class.connection.exec_params(
+        "INSERT INTO #{TABLE_NAME}
+        (name, email) 
+        VALUES ($1, $2) 
+        RETURNING id;", [name, email])
+      @id = res[0]['id'].to_i
+    end
+  end
+
+  def destroy
+    self.class.connection.exec_params(
+      "DELETE FROM #{TABLE_NAME} 
+      WHERE id = $1::int;", [id])
+  end
+
+end
+
+class EmailExists < StandardError
 end
--- contact_list.rb
+++ contact_list.rb
@@ -8,24 +8,50 @@ class ContactList
 
   def run(arguments)
     case arguments.shift
-    when "list"
+    when "all"
       puts Contact.all
-    when "new"
+    when "create"
+      puts "What is the person's full name?"
+      new_name = STDIN.gets.chomp
+      puts "What is their email?"
+      new_email = STDIN.gets.chomp
+      begin
+        contact = Contact.create(new_name, new_email)
+        puts "#{contact} has been added"
+      rescue EmailExists => ex
+        puts ex.message
+      end
+    when "update" 
       puts "What is the person's full name?"
       new_name = $stdin.gets.chomp
       puts "What is their email?"
       new_email = $stdin.gets.chomp
-      puts Contact.create(new_name, new_email)
-    when "show"
-      puts Contact.find(arguments.shift)
+      the_contact = Contact.find(arguments.shift)
+      the_contact.name = new_name
+      the_contact.email = new_email
+      the_contact.save
+      puts "Updated contact to #{the_contact}"
+    when "find"
+      result = Contact.find(arguments.shift)
+      puts result ? result : "No such contact"
+    when "delete"
+      the_contact = Contact.find(arguments.shift)
+      if the_contact
+        the_contact.destroy
+        puts "Contact has been deleted"
+      else
+        puts "The contact cannot be found"
+      end
     when "search"
       puts Contact.search(arguments.shift)
     else
       puts "Here is a list of available commands:"\
-      "\n new - Create a new contact"\
-      "\n list - List all contacts"\
-      "\n show - Show a contact"\
-      "\n search - Search contacts"
+      "\n create - Create a new contact"\
+      "\n all - List all contacts"\
+      "\n find - Show a contact"\
+      "\n search - Search contacts"\
+      "\n update - Update a contact"\
+      "\n delete - Delete a contact"
     end
   end
 end
